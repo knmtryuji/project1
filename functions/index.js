9f403e94d543fee043592f2351384c90c2831883
knmtryuji <kaneryu1029@gmail.com>
2024-07-06 22:15:09 +0900

    test

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-
+export { onAuth } from "./triggers/onAuth";
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
