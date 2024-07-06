1afe587256b4c8e086d77a2dda532dba8df846e9
knmtryuji <kaneryu1029@gmail.com>
2024-07-06 22:14:20 +0900

    test

---
+++ functions/triggers/onAuth.js
@@ -0,0 +1,21 @@
+import * as functions from "firebase-functions";
+import { firestore } from "firebase-admin";
+
+export const onAuth = {
+  createdAuthUser: functions.auth.user().onCreate(async (record) => {
+    const uid = record.uid;
+    const email = record.email;
+    // userコレクションに追加
+    console.log(`create user account: ${email}`);
+    console.log(`uid: ${uid}`);
+    console.log(`email: ${email}`);
+    functions.logger.info(`create user account: ${email}`);
+    functions.logger.info(`uid: ${uid}`);
+    functions.logger.info(`email: ${email}`);
+    firestore().collection("users").doc(uid).set({
+      email: email,
+      createAt: firestore.Timestamp.now(),
+      updateAt: firestore.Timestamp.now(),
+    });
+  }),
+};
